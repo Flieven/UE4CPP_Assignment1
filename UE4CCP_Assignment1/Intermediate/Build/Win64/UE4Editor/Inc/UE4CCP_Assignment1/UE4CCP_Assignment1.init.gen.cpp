72ac490e9083cba3492c46a9e112017d8de8deac
Aledend <theodor.brandt@futuregames.nu>
2020-01-16 15:15:43 +0100

    FireFunctions, Weapon & Barrel Components added

--- UE4CCP_Assignment1/Intermediate/Build/Win64/UE4Editor/Inc/UE4CCP_Assignment1/UE4CCP_Assignment1.init.gen.cpp
+++ UE4CCP_Assignment1/Intermediate/Build/Win64/UE4Editor/Inc/UE4CCP_Assignment1/UE4CCP_Assignment1.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeUE4CCP_Assignment1_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xD2F35184,
-				0xDA7126B0,
+				0x704DDABF,
+				0x33A70571,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
